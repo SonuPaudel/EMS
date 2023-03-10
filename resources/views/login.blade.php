26218dc651bc3b19f4dc3b72fd271bac130ae039
Sonu Paudel <86399801+SonuPaudel@users.noreply.github.com>
2023-03-10 09:47:31 +0545

    Initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <form class="bg-gray-400 p-5 mt-12 w-4/12 mx-auto rounded shadow text-center" action="#" method="POST">
+        <h2 class="font-bold text-2xl my-2">Login</h2>
+        <input type="text" placeholder="Username" class="block p-5 my-5 w-full rounded outlline-none" name="name">
+
+        <input type="password" placeholder="password" class="block p-5 my-5 w-full rounded outlline-none" name="phone">
+
+        <input type="Submit" value="Login" class="p-5 my-5 bg-blue-600 text-white rounded outlline-none" name="Login">
+        <a href="addstudents.php" class="p-5 my-5 bg-red-600 text-white rounded outlline-none"> Cancle </a>
+
+
+
+
+
+    </form>
+
+
+    </form>
+
+</body>
+
+</html>
